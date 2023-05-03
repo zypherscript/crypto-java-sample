938a9a9aeb451298bae932f3f6ba7bb2211c2437
zypherscript <kitti.kthp@gmail.com>
2023-05-04 02:03:36 +0700

    symmetric encryption demo

--- src/main/java/com/example/cryptodemo/CryptodemoApplication.java
+++ src/main/java/com/example/cryptodemo/CryptodemoApplication.java
@@ -1,8 +1,15 @@
 package com.example.cryptodemo;
 
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
@@ -19,7 +26,8 @@ public class CryptodemoApplication {
   }
 
   @Bean
-  CommandLineRunner runner(HashingDemo hashingDemo) {
+  CommandLineRunner runner(HashingDemo hashingDemo,
+      SymmetricEncryptionDemo symmetricEncryptionDemo) {
     return args -> {
       var random = new SecureRandom();
       var salt = new byte[16];
@@ -29,6 +37,8 @@ public class CryptodemoApplication {
       hashingDemo.hashText("sheesh", salt); //deterministic
       hashingDemo.hashText("she3sh", salt); //pseudorandom
       hashingDemo.hashText("sheeeeeeeeeeeeeesh", salt); //fixed length
+
+      symmetricEncryptionDemo.symmetricEncrypt();
     };
   }
 }
@@ -37,12 +47,25 @@ public class CryptodemoApplication {
 @Slf4j
 class Utils {
 
-  static String convertBytes(byte[] digest) {
+  static String convertBytes(byte[] bytes) {
     var sb = new StringBuilder();
-    for (byte b : digest) {
+    for (byte b : bytes) {
       sb.append(String.format("%02x", b));
     }
     return sb.toString();
+  }
+
+  static String decodeBytes(byte[] bytes) {
+    var hexString = convertBytes(bytes);
+    var decodedBytes = new byte[hexString.length() / 2];
+
+    for (int i = 0; i < decodedBytes.length; i++) {
+      int index = i * 2;
+      int j = Integer.parseInt(hexString.substring(index, index + 2), 16);
+      decodedBytes[i] = (byte) j;
+    }
+
+    return new String(decodedBytes, StandardCharsets.UTF_8);
   }
 }
 
@@ -56,5 +79,30 @@ class HashingDemo {
     var digest = messageDigest.digest(str.getBytes());
     log.info("Input: " + str);
     log.info("Digest: " + Utils.convertBytes(digest));
+  }
+}
+
+@Component
+@Slf4j
+class SymmetricEncryptionDemo {
+
+  public void symmetricEncrypt()
+      throws NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException,
+      BadPaddingException, NoSuchPaddingException {
+    var generator = KeyGenerator.getInstance("AES");
+    generator.init(192);
+    var key = generator.generateKey();
+
+    var input = "Sheesh".repeat(16).getBytes();
+    log.info("Input: " + Utils.decodeBytes(input));
+
+    var cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+    cipher.init(Cipher.ENCRYPT_MODE, key);
+    var encryptedOutput = cipher.doFinal(input);
+    log.info("Encrypted Output: " + Utils.convertBytes(encryptedOutput));
+
+    cipher.init(Cipher.DECRYPT_MODE, key);
+    var decryptedOutput = cipher.doFinal(encryptedOutput);
+    log.info("Decrypted Output: " + Utils.decodeBytes(decryptedOutput));
   }
 }
